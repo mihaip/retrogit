2024e06a1f9195c9de67f40ff66125920c735202
Mihai Parparita <mihai@persistent.info>
2014-07-19 15:23:35 -0700

    Add a basic HTTP cache in front of the GitHub API to speed up development.
    Loosely inspired by https://github.com/gregjones/httpcache except we don't want
    to follow HTTP semantics, and instead cache all requests.

--- app/githop.go
+++ app/githop.go
@@ -1,16 +1,21 @@
 package githop
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"strings"
 	"time"
 
 	"appengine"
+	"appengine/memcache"
 	"appengine/urlfetch"
 
 	"code.google.com/p/goauth2/oauth"
@@ -168,9 +173,14 @@ func githubOAuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
 
 func githubOAuthTransport(r *http.Request) *oauth.Transport {
 	appengineContext := appengine.NewContext(r)
+	appengineTransport := &urlfetch.Transport{Context: appengineContext}
+	cachingTransport := &CachingTransport{
+		Transport: appengineTransport,
+		Context:   appengineContext,
+	}
 	return &oauth.Transport{
 		Config:    &githubOauthConfig,
-		Transport: &urlfetch.Transport{Context: appengineContext},
+		Transport: cachingTransport,
 	}
 }
 
@@ -193,4 +203,54 @@ func decodeOAuthToken(tokenEncoded string) (*oauth.Token, error) {
 		return nil, err
 	}
 	return &token, nil
+}
+
+// Simple http.RoundTripper implementation which wraps an existing transport and
+// caches all responses for GET and HEAD requests. Meant to speed up the
+// iteration cycle during development.
+type CachingTransport struct {
+	Transport http.RoundTripper
+	Context   appengine.Context
+}
+
+func (t *CachingTransport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+	if req.Method != "GET" && req.Method != "HEAD" {
+		return t.Transport.RoundTrip(req)
+	}
+	cacheKey := "CachingTransport:" + req.URL.String() + "#"
+	authorizationHeaders, ok := req.Header["Authorization"]
+	if ok {
+		cacheKey += strings.Join(authorizationHeaders, "#")
+	} else {
+		cacheKey += "Unauthorized"
+	}
+
+	cachedRespItem, err := memcache.Get(t.Context, cacheKey)
+	if err != nil && err != memcache.ErrCacheMiss {
+		t.Context.Errorf("Error getting cached response: %v", err)
+		return t.Transport.RoundTrip(req)
+	}
+	if err == nil {
+		cacheRespBuffer := bytes.NewBuffer(cachedRespItem.Value)
+		resp, err := http.ReadResponse(bufio.NewReader(cacheRespBuffer), req)
+		if err == nil {
+			return resp, nil
+		} else {
+			t.Context.Errorf("Error readings bytes for cached response: %v", err)
+		}
+	}
+	resp, err = t.Transport.RoundTrip(req)
+	if err != nil {
+		return
+	}
+	respBytes, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		t.Context.Errorf("Error dumping bytes for cached response: %v", err)
+		return resp, nil
+	}
+	err = memcache.Set(t.Context, &memcache.Item{Key: cacheKey, Value: respBytes})
+	if err != nil {
+		t.Context.Errorf("Error setting cached response: %v", err)
+	}
+	return resp, nil
 }
