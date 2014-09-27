976beb08ccf414447faf75f43b557e9967d0c5e9
Mihai Parparita <mihai@persistent.info>
2014-09-27 16:06:34 -0700

    Move settings JavaScript to a separate file.

---
+++ app/static/settings.js
@@ -0,0 +1,26 @@
+function updateWeeklyDayContainer() {
+  var frequencyNode = document.getElementById("frequency");
+  var weeklyDayContainerNode = document.getElementById("weekly-day-container");
+  if (frequencyNode.value == "weekly") {
+    weeklyDayContainerNode.style.display = "inline";
+  } else {
+    weeklyDayContainerNode.style.display = "none";
+  }
+}
+
+function updateReposContainer() {
+  var includedReposNode = document.getElementById("included-repos");
+  var reposContainerNode = document.getElementById("repos-container");
+  if (includedReposNode.value == "some") {
+    reposContainerNode.style.display = "block";
+  } else {
+    var repoCheckboxes = document.querySelectorAll(".repo input[type=checkbox]");
+    for (var i = 0; i < repoCheckboxes.length; i++) {
+      repoCheckboxes[i].checked = true;
+    }
+    reposContainerNode.style.display = "none";
+  }
+}
+
+updateWeeklyDayContainer();
+updateReposContainer();
