1b22bd6cfc5d5ede55f365f8da282717929a583d
Mihai Parparita <mihai@persistent.info>
2023-06-26 23:07:29 -0700

    Simplify deployment script
    We started to use Go modules (with 8fc40ed0084af21c5a92820b5033653847901bbe)
    and thus don't need to do any hacks before deploying.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,5 @@
 
 # With the Go 1.11 runtime, if we're not using modules, all source (including
 # the app itself) must live under GOPATH. Copy it there before deploying.
-DEST="$GOPATH/src/retrogit"
-rm -rf $DEST
-cp -r app $DEST
-cd $DEST
+cd app
 gcloud app deploy --project retro-git app.yaml
